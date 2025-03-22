b42594177f5bf5433558b159b7a4e0ee401d8826
evagupta04 <111646381+evagupta04@users.noreply.github.com>
2025-03-22 19:48:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (30.09%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -10,6 +10,15 @@ class Solution {
         int newDigits[] = new int[digits.length+1];
         newDigits[0] =1;
         return newDigits;
-    }
-     
+    }   
 }
+
+/**
+NOTE: Array is initialized automatically to zero
+Note, we have to increment last number, but if incrementing changes large integer,
+then we have to change entire array
+EX: [2399], [2391], [9], [99], [2389], [999] */
+/*
+Time complexity: O(n)
+Space complexity: O(1)
+*/
