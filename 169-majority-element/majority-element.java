df7ce4199d8121cad9e07db96114d1d6487e8681
evagupta04 <111646381+evagupta04@users.noreply.github.com>
2025-03-25 00:15:37 +0530

    Time: 1 ms (99.87%) | Memory: 53.7 MB (8.73%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,6 +1,31 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = (nums.length)/2;
+        int count = 0;
+        int candidate = 0;
+        for(int num : nums) {
+            if(count == 0) {
+                candidate = num;
+            }
+            if(num == candidate) {
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+}
+
+/*
+if an element occurs more than n/2 times in the array (where n is the size of the array), it will always occupy the middle position when the array is sorted
+Basic Solution: using sorting, O(nlogn)
+Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n/2];
+-------------------------------
+Time complexity: O(n), using Hashmap
+int n = (nums.length)/2;
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i<nums.length; i++) {
            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
@@ -11,5 +36,5 @@ class Solution {
             }
         }
         return 0;
-    }
-}
+-------------------------------------
+*/
