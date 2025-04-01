a9994d8f1c61deb5b4dd9a4ab7d80cd4484c338a
evagupta04 <111646381+evagupta04@users.noreply.github.com>
2025-04-01 20:51:35 +0530

    Time: 7 ms (97.71%) | Memory: 46.8 MB (25.37%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,6 +1,30 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+        List<int[]> output = new ArrayList<>();
+        int[] prev = intervals[0];
+
+        for(int i=1; i<intervals.length; i++) {
+            int[] current = intervals[i];
+            if(prev[1] >= current[0]) {
+                prev[1] = Math.max(prev[1], current[1]);
+            } 
+            else {
+                output.add(prev);
+                prev = intervals[i];
+            }
+        }
+        output.add(prev);
+        return output.toArray(new int[output.size()][]);
+    }
+}
+
+
+/*
+Time complexity: O(nlogn)
+Space complexity: O(n)
+
+Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
 
         List<int[]> merged = new ArrayList<>();
         int[] prev = intervals[0];
@@ -17,5 +41,5 @@ class Solution {
         }
         merged.add(prev);
         return merged.toArray(new int[merged.size()][]);
-    }
-}
+
+*/
